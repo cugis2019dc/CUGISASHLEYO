2cf171f75aacc372b94b05d5b880a79a375a4513
ashley-o <53440511+ashley-o@users.noreply.github.com>
2019-07-29 13:56:00 -0400

    Add files via upload

---
+++ python.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+def multiply(a,b):
+    multiply = a*b
+    print(multiply)
+multiply(4,5) 
+
+def sum(a,b,c):
+    sum = a+b+c 
+    print(sum)
+sum(88,99,-5)
+
+def area(a,b,c):
+    area = a*b*c
+    print('The area of the triangle with base of',b, 'and height of',c, 'is',area)
+area(0.5,7,8)
